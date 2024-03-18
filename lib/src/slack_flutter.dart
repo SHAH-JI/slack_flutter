004e13c7808f4901f3c21b07e9dc1be8fd72a054
SHAH-JI <shahji1998@gmail.com>
2024-03-18 20:18:24 +0500

    feat: add fetch user by email

--- lib/src/slack_flutter.dart
+++ lib/src/slack_flutter.dart
@@ -1,13 +1,28 @@
 import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 /// A [class] to interact with Slack API.
 class SlackFlutter {
   /// A [Default Constructor] to initialize the [SlackFlutter] class.
   SlackFlutter({required String authToken}) {
+    _dio.interceptors.add(
+      PrettyDioLogger(
+        responseBody: false,
+        request: false,
+      ),
+    );
     _dio.options.headers['Authorization'] = 'Bearer $authToken';
-    _dio.options.baseUrl = 'https://slack.com/api';
   }
 
   /// A [final] instance of Dio.
-  final Dio _dio = Dio();
+  final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://slack.com/api/',
+    ),
+  );
+
+  /// [Function] that search slack user by email.
+  Future<Response<dynamic>> fetchUserByEmail({required String email}) async {
+    return _dio.get('users.lookupByEmail', queryParameters: {'email': email});
+  }
 }
